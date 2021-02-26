08a33ff53d11ebe119c3e295f166d6d47b8f2759
Robin Edwards <robinedwards1987@gmail.com>
2021-02-26 13:31:56 +0000

    created drop down for citys

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -4,6 +4,7 @@ const AddProperty = () => {
   const initialState = {
     fields: {
       title: "",
+      city: "Manchester",
     },
   };
   const [fields, setFields] = useState(initialState.fields);
@@ -29,6 +30,19 @@ const AddProperty = () => {
             value={fields.title}
             onChange={handleFieldChange}
           />
+        </label>
+        <label htmlFor="city">
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
         </label>
         <button type="submit">Add</button>
       </form>
