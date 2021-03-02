47c5969158628539de9cbe4da236b3f4ff40a4c5
Robin Edwards <robinedwards1987@gmail.com>
2021-03-02 11:56:47 +0000

    Added snapshot tests for ,<Alert />

--- src/components/__tests__/Alert.test.js
+++ src/components/__tests__/Alert.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Alert from "../components/Alert";
+import Alert from "../Alert";
 
 test("displays an error message", () => {
   const { getByText } = render(<Alert message="Error!" />);
@@ -12,4 +12,13 @@ test("displays an success message", () => {
   const { getByText } = render(<Alert message="Success!!!" success />);
 
   expect(getByText(/Success/).textContent).toBe("Success!!!");
+});
+
+//need to check over theses tests this evening 
+test("does not render an error or a success message if message props is empty", () => {
+  const { getByText, asFragment } = render(<Alert message="Error!" />);
+  const alert = asFragment();
+  expect(alert).toMatchSnapshot();
+  expect(getByText(/Error/)).toMatchSnapshot("Error!");
+  expect(asFragment()).toMatchSnapshot();
 });
