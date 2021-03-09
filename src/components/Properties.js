0f1bc81b9fa289eb12c03b1acb0aeea04bbbb897
Robin Edwards <robinedwards1987@gmail.com>
2021-03-09 10:24:27 +0000

    Last Step

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -46,7 +46,6 @@ const Properties = (userID) => {
   if (!alert.isSuccess) {
     return <Alert message={alert.message} success={alert.isSuccess} />;
   }
-  /*---------save----------*/
   const handleSaveProperty = (propertyId) => {
     axios.post("http://localhost:4000/api/v1/Favourite", {
       propertyListing: propertyId,
